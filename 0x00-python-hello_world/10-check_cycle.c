3c8aa7b974f7b558577978d17fb40081ee8e46c1
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-03-08 05:40:46 +0000

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,20 +6,21 @@
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+listint_t *slow = list;
+listint_t *fast = list;
 
-	if (!list)
-		return (0);
+if (!list)
+return (0);
 
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next
-			if (slow == fast)
-				return (1);
-	}
-	return (0);
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
 }
