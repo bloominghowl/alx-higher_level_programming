272716c10aed57ad993a15999633ed2db06bf4d5
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-03-09 00:43:18 +0000

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
-remove_char_at = __import__('101-remove_char_at').remove_char_at
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newStr = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            newStr += str[i]
+    return newStr
