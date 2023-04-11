99df4a8af731cad244a733a3258c2c929dbe3b3b
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-04-11 17:59:00 +0000

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,10 +7,10 @@ Contains definition of class MyInt
 class MyInt(int):
     """Definition of class MyInt that inherits from class int"""
 
-    def __equal__(self, other):
+    def __eq__(self, other):
         """Overrides equals, inverting it"""
         return int(self) != int(other)
 
-    def __nequal__(self, other):
+    def __neq__(self, other):
         """Overrides not-equals, inverting it"""
         return int(self) == int(other)
