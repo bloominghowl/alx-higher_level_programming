584eda80536675c6524c12513e21af05cb3ca95a
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-16 17:39:01 +0000

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,5 @@
-USE hbtn_0c_0; -- Specify the database name
+-- Specify the database name
+USE hbtn_0c_0; 
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
