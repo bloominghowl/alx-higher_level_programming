7dd3c58ca041d1d5ba9f4a7a19af237ea1b42f33
Ayivi <kafuigalley7@gmail.com>
2023-05-16 10:49:26 -0700

    lists all rows of a table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+--a script that lists all rows of the table first_table 
+--from the database hbtn_0c_0 in MySQL server.
+
+USE hbtn_0c_0; -- Specify the database name
+
+SELECT * FROM first_table;
+
