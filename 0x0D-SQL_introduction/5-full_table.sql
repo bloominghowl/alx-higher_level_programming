f6397dc3840d1bb757b3deb50ffa960a4b7e0d4a
Ayivi <kafuigalley7@gmail.com>
2023-05-16 10:35:39 -0700

    print full description of first table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+USE hbtn_0c_0; -- Specify the database name
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' -- Specify the database name
+  AND TABLE_NAME = 'first_table'; -- Specify the table name
+
