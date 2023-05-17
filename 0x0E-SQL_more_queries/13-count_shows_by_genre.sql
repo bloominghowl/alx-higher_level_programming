51f45d5e7d4e3ad1a00ccc87243ff9fc95e202ba
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-17 20:37:32 +0000

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,6 @@
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
