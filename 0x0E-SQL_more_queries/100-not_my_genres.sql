14b36e656e2730bea3cca143791c1424e7ab7e95
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-17 20:50:21 +0000

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,15 @@
 -- Use the hbtn_0d_tvshows database
 
--- Get the genre IDs linked to the show Dexter
-SELECT genre_id
-FROM tv_show_genres
-WHERE show_id = (
-    SELECT id
-    FROM tv_shows
-    WHERE title = 'Dexter'
+-- Use of Join and subquery to get required data
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_genres
+    JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title = 'Dexter'
 )
+ORDER BY tv_genres.name;
