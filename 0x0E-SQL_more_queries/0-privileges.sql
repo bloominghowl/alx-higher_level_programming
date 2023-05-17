91f6dca9ccc627a8c5797395c91a6f195e524f9d
Ayivi <kafuigalley7@gmail.com>
2023-05-17 12:08:28 -0700

    initial

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Connect to the MySQL server
+USE mysql;
+
+-- Query for user_0d_1
+SELECT * FROM user WHERE user = 'user_0d_1';
+
+-- Query for user_0d_2
+SELECT * FROM user WHERE user = 'user_0d_2';
