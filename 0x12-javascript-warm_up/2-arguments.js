04849d65ab7245a1278710015aa7ae7ba5eeac29
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-06-12 22:42:52 +0000

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argc = process.argv.length;
+const argsCount = process.argv.length - 2;
 
-if (argc === 2) {
-  console.log('No argument');
-} else if (argc === 3) {
-  console.log('Argument found');
-} else if (argc > 3) {
-  console.log('Arguments found');
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
