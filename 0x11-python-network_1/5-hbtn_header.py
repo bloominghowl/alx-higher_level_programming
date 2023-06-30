73aa05c0bddc2fb0f57325657f61c5b71665e313
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-06-30 12:41:29 +0000

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-Sends a request to a given URL and displays the value of the X-Request-Id variable in the response header.
-"""
+"""Sends a request to a given URL and displays the value
+of the X-Request-Id variable in the response header."""
+
 
 if __name__ == '__main__':
     import sys
