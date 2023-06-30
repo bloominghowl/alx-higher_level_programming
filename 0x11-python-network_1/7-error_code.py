8472fbe5709aa3cadfb4b9d96237b51596bb0d5e
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-06-30 12:48:42 +0000

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL, displays the body of
-the response, and handles HTTP status codes greater than 
-or equal to 400."""
+"""sends a request to the URL and displays the body of 
+the response."""
 
+if __name__ == "__main__":
+    import sys
+    import requests
 
-import sys
-import requests
-
- response = requests.get(sys.argv[1])
- if response.status_code >= 400:
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
- else:
+    else:
         print(response.text)
