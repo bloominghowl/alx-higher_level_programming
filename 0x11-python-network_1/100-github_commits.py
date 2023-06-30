e18a2da16b9c9abac9d45eebc861f3dc402a3182
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-06-30 13:11:13 +0000

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""
-Takes in GitHub repo and owner name to list 10 commits (from the most recent to oldest).
-"""
-
-import requests
-import sys
+"""Takes in Github repo and owner name to list 10 commits (from the most recent to oldest)"""
 
 if __name__ == "__main__":
-    repository = sys.argv[1]
-    owner = sys.argv[2]
+    import requests
+    import sys
 
-    url = f"https://api.github.com/repos/{}/{}/commits"
-    response = requests.get(url)
-
-    if response.status_code >= 400:
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
         print('None')
     else:
-        for commit in response.json()[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author_name))
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'),
+                                  com.get('commit').get('author').get('name')))
